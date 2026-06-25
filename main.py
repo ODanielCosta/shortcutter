171acfd261d5b29e637bf01832ceac8bf165beb5
ucosta <danielandcosta@gmail.com>
2026-06-25 16:24:50 +0100

    Faz match com hwnd da janela selecionada

--- main.py
+++ main.py
@@ -19,18 +19,28 @@ brave_hwnd = []
 for proc in psutil.process_iter(['pid', 'name']):
     if proc.info["name"]== "brave.exe":
         brave_pid.append(proc.info['pid'])
-#print(brave_pid)
+print("PROCESSOS:", brave_pid)
 
 #3 GetWindowThreadProcessId → liga HWND → PID
-def callback(hwnd, extra):
-    
+def callback(hwnd, extra):    
     pid = win32process.GetWindowThreadProcessId(hwnd)[1]
     if pid in brave_pid and "Default IME" not in win32gui.GetWindowText(hwnd) and "MSCTFIME UI" not in win32gui.GetWindowText(hwnd) and len(win32gui.GetWindowText(hwnd)) > 1:
         #win32gui.IsIconic deteta se está minimizado
         print(f"hwnd: {hwnd} | Minimizado: {win32gui.IsIconic(hwnd)} | {win32gui.GetWindowText(hwnd)} | Processo: {pid}")
 
         brave_hwnd.append(hwnd)
-        print(brave_hwnd)
+        brave_true_hwnd = brave_hwnd[0]
+        print(brave_true_hwnd)
+        print(win32gui.GetForegroundWindow())
+
+        #win32gui.GetForegroundWindow() VAI BUSCAR A JANELA SELECIONADA ATUAL
+        if win32gui.GetForegroundWindow() == brave_true_hwnd:
+            print("Match")
+            foreground_hwnd = brave_true_hwnd
+
+    
+    #if pid in brave_pid:
+    #   print(f"hwnd: {hwnd} | Minimizado: {win32gui.IsIconic(hwnd)} | {win32gui.GetWindowText(hwnd)} | Processo: {pid}")
 
 
 #2 EnumWindows → janelas (HWND (Handle Window))

c2ce49b7cb7200f6ff0949ae57a9fb5a4941b794
ucosta <danielandcosta@gmail.com>
2026-06-25 11:41:55 +0100

    PID + hwnd completo

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import keyboard as kb
 import pygetwindow as gw
 import pyautogui as autog
-import psutil #Vai buscar processos e info
-import win32gui
-import win32process
+import psutil #Vai buscar processos
+import win32gui #Vai buscar o Handle Window
+import win32process #Liga o Handle Window a um processo
 import win32api
 
 print("  +------------------------+")
@@ -12,15 +12,28 @@ print("  |       Version 1.0      |")
 print("  +------------------------+")
 
 #PROCESSOS###############
+brave_pid = []
+
+#1 psutil → processos (PID)
 for proc in psutil.process_iter(['pid', 'name']):
     if proc.info["name"]== "brave.exe":
-        print(proc.info)
+        brave_pid.append(proc.info['pid'])
+print(brave_pid)
 
-for janela in win32gui.EnumWindows(hwnd,extra):
-    print(janela)
+#3 GetWindowThreadProcessId → liga HWND → PID
+def callback(hwnd, extra):
+    pid = win32process.GetWindowThreadProcessId(hwnd)[1]
+    if pid in brave_pid:
+        print(hwnd, pid)
+
+#2 EnumWindows → janelas (HWND (Handle Window))
+win32gui.EnumWindows(callback,None)
+
+
 
 #SHORTCUTS###############
 #
+"""
 def win1 ():
     autog.hotkey('win','1')
     print("win + 1")
@@ -29,3 +42,4 @@ kb.add_hotkey("shift+s",win1)
 
 
 kb.wait()
+"""
